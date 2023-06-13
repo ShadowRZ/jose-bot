bfa52cddfd1d7dfc4c8c973dd1b0584298bf3b29
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-06-13 20:30:45 +0800

    Handle KeyboardInterrupt

--- jose_bot/__init__.py
+++ jose_bot/__init__.py
@@ -1,10 +1,13 @@
 import asyncio
+import logging
 import sys
 
 # Check that we're not running on an unsupported Python version.
 if sys.version_info < (3, 5):
     print("jose_bot requires Python 3.5 or above.")
     sys.exit(1)
+
+logger = logging.getLogger(__name__)
 
 
 def run():
@@ -15,3 +18,5 @@ def run():
         asyncio.get_event_loop().run_until_complete(main.main())
     except ImportError as e:
         print("Unable to import jose_bot.main:", e)
+    except KeyboardInterrupt:
+        logger.info("Bye!")
